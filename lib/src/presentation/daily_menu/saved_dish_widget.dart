116034e75a7e758ebd633477ae836c2522565aef
Bogdan M <bogdancons99@gmail.com>
2021-05-21 09:30:41 +0300

    resolve warning with isPressed

--- lib/src/presentation/daily_menu/saved_dish_widget.dart
+++ lib/src/presentation/daily_menu/saved_dish_widget.dart
@@ -5,48 +5,56 @@ import 'package:food_delivery_admin/src/models/index.dart';
 import 'package:food_delivery_admin/src/presentation/app_routes.dart';
 
 class SavedDishWidget extends StatefulWidget {
-  SavedDishWidget({required this.dish,required this.categoryId, required this.isPressed});
+  const SavedDishWidget({required this.dish, required this.categoryId, required this.isPressed});
 
   final Dish dish;
   final String categoryId;
-  bool isPressed;
+  final bool isPressed;
 
   @override
   _SavedDishWidgetState createState() => _SavedDishWidgetState();
 }
 
 class _SavedDishWidgetState extends State<SavedDishWidget> {
+  bool isPressed = false;
 
+  @override
+  void initState() {
+    super.initState();
+    isPressed = widget.isPressed;
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(widget.dish.name),
       subtitle: Text(widget.dish.description ?? ''),
-      onTap: (){
-          Navigator.pushNamed(context, AppRoutes.addSavedDish, arguments: widget.dish);
+      onTap: () {
+        Navigator.pushNamed(context, AppRoutes.addSavedDish, arguments: widget.dish);
       },
       trailing: Column(
         children: <Widget>[
 //          Text(widget.dish.price.toString()),
           IconButton(
-              icon: widget.isPressed == false
-                  ? const Icon(Icons.add_circle_outline)
-                  : const Icon(
-                      Icons.check_circle,
-                      color: Colors.green,
-                    ),
-              onPressed: () {
-                setState(() {
-                  widget.isPressed = !widget.isPressed;
-
-                });
-                if (widget.isPressed){
-                  StoreProvider.of<AppState>(context).dispatch(UpdateDishes(categoryId: widget.categoryId, add: widget.dish));
-                }else{
-                  StoreProvider.of<AppState>(context).dispatch(UpdateDishes(categoryId: widget.categoryId, remove: widget.dish));
-                }
-              },)
+            icon: isPressed == false
+                ? const Icon(Icons.add_circle_outline)
+                : const Icon(
+                    Icons.check_circle,
+                    color: Colors.green,
+                  ),
+            onPressed: () {
+              setState(() {
+                isPressed = !isPressed;
+              });
+              if (isPressed) {
+                StoreProvider.of<AppState>(context)
+                    .dispatch(UpdateDishes(categoryId: widget.categoryId, add: widget.dish));
+              } else {
+                StoreProvider.of<AppState>(context)
+                    .dispatch(UpdateDishes(categoryId: widget.categoryId, remove: widget.dish));
+              }
+            },
+          )
         ],
       ),
     );
